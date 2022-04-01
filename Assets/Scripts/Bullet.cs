1fe920fb12addd68f190b230dcbe5678524306b0
Signor-Koala <jeffrey.samuel@protonmail.com>
2022-04-02 02:56:25 +0530

    Player can now shoot. And power ups

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5f;
+    public Transform plr;
+    public float reload = 0.5f;
+    public int damage = 20;
+
+    private Vector2 speedVec;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        speedVec = new Vector2(speed*(rb.position.x - plr.position.x),speed*(rb.position.y - plr.position.y));
+        rb.velocity = speedVec;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject projectile;
+    
+    private Rigidbody2D rb;
+
+    private void Start()
+    { 
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.name);
+        
+        PlayerController player = col.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.currenProj = projectile;
+            Destroy(gameObject);
+        }
+        
+    }
+
+
+}
